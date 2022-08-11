fc1176fdfac5811a747a643f9feb4d93604215d0
daxkongjh <dax.k@live.com>
2022-08-11 20:05:12 +0800

    update

--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -9,3 +9,14 @@
                                               // "Java > Python."
                                               // "I spilled Java all over my paper."
                                               // "My dog ate my Java." 
+
+
+                                              
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Java > Python.");
+        System.out.println("I spilled Java all over my paper.");
+        System.out.println("My dog ate my Java.");
+    }
+}
